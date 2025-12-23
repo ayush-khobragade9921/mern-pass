887bbd7d99bae629b899043d697d679aac557262
ayush-khobragade9921 <ayushkhobmicro@outlook.com>
2025-12-23 23:05:15 +0530

    dep

--- frontend/pass-managment/src/services/api.js
+++ frontend/pass-managment/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://visitor-backend-fxpw.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
